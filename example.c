9b784788d5b1c624b983f1f18887ebb359f1226c
Dalibor Baco <xbaco@fi.muni.cz>
2025-05-15 15:27:43 +0200

    feat: c vuln test

--- example.c
+++ example.c
@@ -12,6 +12,13 @@ int main(void) {
     uint8_t iv[16];
     uint8_t ciphertext[32];
     uint8_t plaintext[32];
+
+    char str1[]="Sample string";
+  char str2[40];
+  char str3[40];
+  strcpy (str2,str1);
+  strcpy (str3,"copy successful");
+  printf ("str1: %s\nstr2: %s\nstr3: %s\n",str1,str2,str3);
   
     // iv || ciphertext
     recv_message(message);
